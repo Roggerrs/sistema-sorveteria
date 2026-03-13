067c1d552994d8e904d5f2ba0234e58a74b0c622
Roggerrs <rogger6412@gmail.com>
2026-03-13 20:43:39 -0300

    CorsConfig.java Fazendo servidor localmente no docker e depois cloudflare tunnel

--- src/main/java/br/com/sorveteria/sistema_sorveteria/config/CorsConfig.java
+++ src/main/java/br/com/sorveteria/sistema_sorveteria/config/CorsConfig.java
@@ -1,20 +1,31 @@
-//package br.com.sorveteria.sistema_sorveteria.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-//                        "https://sorveteria-frontend-chi.vercel.app",
-//                        "http://localhost:5173"
-//                )
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*");
-//    }
-//}
+package br.com.sorveteria.sistema_sorveteria.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:5173"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+
+            }
+
+        };
+    }
+}
